e20a0a2a6eca2386bc60fdf21bd5f6b87e2de192
LGI7\meett <torins@naver.com>
2018-11-25 10:42:59 +0900

    배포판

--- client/js/function_ajax.js
+++ client/js/function_ajax.js
@@ -10,7 +10,7 @@ function ajax_selectmanitto(name,callback){
 		},
 		success:function(data){
 			var result = JSON.parse(data); 
-         	document.getElementById("manitoname").value = result[0].manitto;
+         	document.getElementById("manitoname").value = document.getElementById("yourname").value +"님의 마니또는 "+ result[0].manitto+"님 입니다!";
 			if(callback) callback(data);
 		}
 	});
